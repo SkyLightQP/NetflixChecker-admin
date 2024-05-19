caf1bd0495723950b873b171eb425d114a70a1c7
Daegyeom Ha <combbm@gmail.com>
2024-05-19 20:34:41 +0900

    chore: add prettier and update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  globals: {
+    JSX: true
+  },
+  rules: {
+    'arrow-body-style': 'off',
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'linebreak-style': 'off',
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off'
+  }
+};
