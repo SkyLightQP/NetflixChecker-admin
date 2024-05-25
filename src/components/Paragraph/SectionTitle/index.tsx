3744a20d0104037253f60a1fb4795235c30d178f
Daegyeom Ha <combbm@gmail.com>
2024-05-25 14:45:36 +0900

    feat: add `className` prop in SectionTitle component

--- src/components/Paragraph/SectionTitle/index.tsx
+++ src/components/Paragraph/SectionTitle/index.tsx
@@ -1,5 +1,10 @@
-import { FC, PropsWithChildren } from 'react';
+import { FC, HTMLAttributes, PropsWithChildren } from 'react';
+import { cn } from '@/utils/cn';
 
-export const SectionTitle: FC<PropsWithChildren> = ({ children }) => {
-  return <h1 className="font-bold text-3xl mb-8">{children}</h1>;
+export const SectionTitle: FC<
+  PropsWithChildren<HTMLAttributes<HTMLHeadingElement>>
+> = ({ children, className }) => {
+  return (
+    <h1 className={cn('font-bold text-3xl mb-8', className)}>{children}</h1>
+  );
 };
