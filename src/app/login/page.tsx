ab197d289f6a1156d6220fd5959c1d4b40eb3186
Daegyeom Ha <combbm@gmail.com>
2024-06-02 15:57:24 +0900

    feat: implement login logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,31 @@
+'use client';
+
 import { FC } from 'react';
 import { Button, Input, Spacer } from '@nextui-org/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons';
+import { api } from '@/utils/fetch-api';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
 
 const Page: FC = () => {
+  const router = useRouter();
+
+  const login = async (formData: FormData) => {
+    const rawFormData = {
+      email: formData.get('email'),
+      password: formData.get('password')
+    };
+
+    const { ok, json } = await api('/auth/login', 'POST', rawFormData);
+    if (!ok) {
+      toast(json.message);
+      return;
+    }
+
+    router.push('/');
+  };
+
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="flex flex-col items-center">
@@ -12,13 +34,20 @@ const Page: FC = () => {
           &nbsp;관리자 페이지
         </h1>
         <Spacer y={6} />
-        <Input className="w-80" type="email" label="이메일" />
-        <Spacer y={4} />
-        <Input className="w-80" type="password" label="비밀번호" />
-        <Spacer y={4} />
-        <Button color="primary" className="w-60">
-          로그인
-        </Button>
+        <form action={login} className="flex flex-col items-center">
+          <Input className="w-80" type="email" label="이메일" name="email" />
+          <Spacer y={4} />
+          <Input
+            className="w-80"
+            type="password"
+            label="비밀번호"
+            name="password"
+          />
+          <Spacer y={4} />
+          <Button type="submit" color="primary" className="w-60">
+            로그인
+          </Button>
+        </form>
       </div>
     </div>
   );
