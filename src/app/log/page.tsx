7841c8159d4e448024ef473dc61c7098df67b8ab
Daegyeom Ha <combbm@gmail.com>
2024-11-16 16:29:10 +0900

    feat: goto bottom when log refreshed

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { SectionTitle } from '@/components/Paragraph/SectionTitle';
 import { Code } from '@nextui-org/code';
 import { Button } from '@nextui-org/react';
@@ -10,11 +10,16 @@ import { api } from '@/utils/fetch-api';
 
 const Page: FC = () => {
   const [logs, setLogs] = useState<string[]>(['로그가 없습니다.']);
+  const logRef = useRef<HTMLElement | null>(null);
 
   const fetchData = useCallback(async () => {
     const { json } = await api<{ result: string[] }>('/metrics/logs', 'GET');
     if (json !== null) {
       setLogs(json.result);
+
+      if (logRef.current) {
+        logRef.current.scrollTop = logRef.current.scrollHeight;
+      }
     }
   }, []);
 
@@ -32,7 +37,10 @@ const Page: FC = () => {
           </Button>
         </div>
       </SectionTitle>
-      <Code className="w-[calc(100vw-26rem)] h-[calc(100vh-20rem)] overflow-auto">
+      <Code
+        className="w-[calc(100vw-26rem)] h-[calc(100vh-20rem)] overflow-auto"
+        ref={logRef}
+      >
         {logs.map((log, index) => (
           // eslint-disable-next-line react/no-array-index-key
           <p key={`log-${index}`}>{log}</p>
