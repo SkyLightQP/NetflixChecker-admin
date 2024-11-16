bece53a38fcc4d021cc14e67cd0647dab99a261b
Daegyeom Ha <combbm@gmail.com>
2024-11-16 16:24:41 +0900

    feat: implement log page

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -1,13 +1,42 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useCallback, useEffect, useState } from 'react';
 import { SectionTitle } from '@/components/Paragraph/SectionTitle';
 import { Code } from '@nextui-org/code';
+import { Button } from '@nextui-org/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRefresh } from '@fortawesome/free-solid-svg-icons';
+import { api } from '@/utils/fetch-api';
 
 const Page: FC = () => {
+  const [logs, setLogs] = useState<string[]>(['로그가 없습니다.']);
+
+  const fetchData = useCallback(async () => {
+    const { json } = await api<{ result: string[] }>('/metrics/logs', 'GET');
+    if (json !== null) {
+      setLogs(json.result);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData().then();
+  }, [fetchData]);
+
   return (
     <>
-      <SectionTitle>로그</SectionTitle>
-      <Code className="w-[calc(100vw-26rem)]">
-        <p>준비중</p>
+      <SectionTitle className="flex justify-between">
+        <span>로그</span>
+        <div>
+          <Button color="primary" onClick={fetchData}>
+            <FontAwesomeIcon icon={faRefresh} />
+          </Button>
+        </div>
+      </SectionTitle>
+      <Code className="w-[calc(100vw-26rem)] h-[calc(100vh-20rem)] overflow-auto">
+        {logs.map((log, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <p key={`log-${index}`}>{log}</p>
+        ))}
       </Code>
     </>
   );
